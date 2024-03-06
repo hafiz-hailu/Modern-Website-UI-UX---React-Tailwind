58f3692b3ee0cbc410a43beea7436d24e9991d09
Hafiz Hailu <hafizhailu.dev@gamil.com>
2024-03-06 15:25:19 +0300

    Adding Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import styles from './style';
+import {
+  Billing,
+  Business,
+  Button,
+  CarDeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Footer,
+  GatStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonial,
+} from './components';
 
-const App = () => {
-  return <div>App</div>;
-};
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    {/* Navbar */}
+    <Navbar />
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>NAVBAR</div>
+    </div>
+
+    {/* Hero */}
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    {/* Multiple sections */}
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}></div>
+    </div>
+  </div>
+);
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from './Billing';
+import Business from './Business';
+import Button from './Button';
+import CarDeal from './CarDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+import FeedbackCard from './FeedbackCard';
+import Footer from './Footer';
+import GatStarted from './GetStarted';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Stats from './Stats';
+import Testimonial from './Testimonial';
+
+export {
+  Billing,
+  Business,
+  Button,
+  CarDeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Footer,
+  GatStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonial,
+};
